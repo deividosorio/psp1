05114d4f2fb0e7e6d3a350f39e64d61a0070fda8
Deivid Alexander Osorio Barrera <deividosorio@MacBook-de-Deivid.local>
2015-02-10 00:33:30 -0500

    Psp1, ajuste de presentación mostrando unit test.

--- src/main/java/edu/uniandes/ecos/psp1/vista/MainView.java
+++ src/main/java/edu/uniandes/ecos/psp1/vista/MainView.java
@@ -53,6 +53,19 @@ public class MainView {
         resp.getWriter().println("<b>Valor R2 :</b>   " + formatter.format(r2) + "<br>");
         resp.getWriter().println("<b>Valor Rxy: </b>  " + formatter.format(rxy) + "<br>");
         resp.getWriter().println("<b>Predicción: </b> " + formatter.format(yk) + "<br>");
+        
+        resp.getWriter().println("<hr>");
+        resp.getWriter().println("<h2>Test Realizados</h2>");
+        resp.getWriter().println("<b>Lista test1: <br></b>130,186-650,699-"
+                + "99,132-150,272-128,291-302,331-95,199-945,1890-368,788-961,1601<br>");
+        resp.getWriter().println("<b>Lista test2: <br></b>130,15-650,69.9-"
+                + "99,6.5-150,22.4-128,28.4-302,65.9-95,19.4-945,198.7-368,38.8-961,138.2<br>");
+        resp.getWriter().println("<b>Lista test3: <br></b>163,186-765,699-"
+                + "141,132-166,272-137,291-355,331-136,199-1206,1890-433,788-1130,1601<br>");
+        resp.getWriter().println("<b>Lista test4: <br></b>163,15-765,69.9-"
+                + "141,6.5-166,22.4-137,28.4-355,65.9-136,19.4-1206,198.7-433,38.8-1130,138.2<br>");
+        
+        
     }
 
     public static void error(HttpServletRequest req, HttpServletResponse resp)
